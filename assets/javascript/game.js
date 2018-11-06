12503b1d1707305c0cd680fb6893d0b4f39bb034
lmccray <lmccray@uhin.org>
2018-11-06 11:12:52 -0700

    got word displaying and updating on input

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,28 +1,84 @@
-
-var userText = document.getElementById("inputdefault");
-
 var words = {
     name: "words",
     wordlist: ["abaft", "rapid", "magic", "spread", "seashore", "account", "rest", "spiky", "decision", "nifty"],
-    getrandomword: function() {
-    var arraylenght = this.wordlist.length;
-    var randomnumber = Math.floor((Math.random() * arraylenght));
-    return this.wordlist[randomnumber];
+    getrandomword: function () {
+        var arraylenght = this.wordlist.length;
+        var randomnumber = Math.floor((Math.random() * arraylenght));
+        return this.wordlist[randomnumber];
     },
-    getwordlength: function(word){
+    getwordlength: function (word) {
         return word.length;
-    } 
+    }
 };
 
+var rounds = 0;
+var guesses = 0;
 var randomword = words.getrandomword();
 var randomwordlength = words.getwordlength(randomword);
-console.log(randomword);
-console.log(words.getwordlength(randomword));
+var currentword = [];
 for (i = 0; i < randomwordlength; i++) {
-    var mydiv = document.getElementById("stuffcontainer");
-    var creatediv = document.createElement("div");
-    creatediv.id = 'wordnumber' + i;
-    mydiv.appendChild(creatediv);
-    creatediv.appendChild(document.createTextNode(i));
+    var loopletter = randomword.charAt(i);
+    currentword.push(loopletter);
+}
+
+var playingarray = [];
+
+function setInitialArray() {
+    for (i = 0; i < randomwordlength; i++) {
+        playingarray.push("_")
+    }
+}
+setInitialArray();
+displayPlayArea();
+
+function updatePlayingArray(value) {
+    for (i = 0; i < randomwordlength; i++) {
+        if (value === currentword[i]) {
+            playingarray[i] = value;
+        }
+    }
 
 }
+
+function clearPlayArea() {
+    var containerdiv = document.getElementById("stuffcontainer");
+    containerdiv.parentNode.removeChild(containerdiv);
+
+}
+
+function displayPlayArea() {
+    for (i = 0; i < randomwordlength; i++) {
+        var maincontainer = document.getElementById("maincontainer");
+        var createul = document.createElement("ul");
+        createul.id = "stuffcontainer";
+        createul.className = "list-inline d-flex justify-content-center";
+        maincontainer.appendChild(createul);
+        var mydiv = document.getElementById("stuffcontainer");
+        var creatediv = document.createElement("li");
+        creatediv.id = 'wordnumber' + i;
+        creatediv.className = "list-inline-item";
+        mydiv.appendChild(creatediv);
+        creatediv.appendChild(document.createTextNode(playingarray[i]));
+    }
+
+}
+function submitMe() {
+    var inputvalue = document.getElementById('inputdefault').value;
+    clearPlayArea();
+    updatePlayingArray(inputvalue);
+    displayPlayArea();
+}
+
+
+
+
+
+
+
+
+console.log(randomword);
+console.log(words.getwordlength(randomword));
+
+
+
+
